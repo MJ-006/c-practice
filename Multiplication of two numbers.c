4f859a776007000d2da4532e40373a3f08d6f4c5
M.Dheeraj <dheerajm21441@gmail.com>
2025-08-25 21:14:39 +0530

    Add files via upload

---
+++ Multiplication of two numbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,multiplication;
+    printf("Enter two integer values :");
+    scanf("%d%d",&a,&b);
+    printf("%d*%d=%d\n",a,b,a*b);
+    return 0;
+}
